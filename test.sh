adbe54482aa3ff98d23991ab6719fcd2e1649cca
David Rueda <davd33@gmail.com>
2026-06-01 13:19:30 +0200

    test.sh - test binary to all asm files

--- test.sh
+++ test.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+function test_asm() {
+  echo "Test ${ASM_BINARY}..."
+  ./target/debug/cpu-instruction-sim "${ASM_BINARY}" > output.txt 2> /dev/null
+
+  # Then compile output with NASM:
+  nasm output.txt
+
+  # Then compare with diff:
+  diff output "$ASM_BINARY" && echo "OK" || echo "KO"
+}
+
 # change the path below to the file to be tested
+export ASM_BINARY='asm/mov_mem_to_reg/simple'
+test_asm
 export ASM_BINARY='asm/mov_mem_to_reg/hard'
-./target/debug/cpu-instruction-sim "$ASM_BINARY" > output.txt 2> /dev/null
-
-# Then compile output with NASM:
-nasm output.txt
-
-# Then compare with diff:
-diff output "$ASM_BINARY"
+test_asm
+export ASM_BINARY='asm/mov_reg_to_reg/simple'
+test_asm
+export ASM_BINARY='asm/mov_reg_to_reg/long'
+test_asm
